1fa141391336b4673955ee8606bbbb5df72008a4
deepaksharmadk <dks847203@gmail.com>
2024-01-27 05:42:29 +0530

    call db

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import databaseConnect from "./db/database.js";
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+
+app.listen(process.env.PORT, async () => {
+  try {
+    const connectdb = await databaseConnect();
+    console.log(`app listening on ${process.env.PORT}`);
+  } catch (error) {
+    console.log("app is not listening on ${process.env.PORT}", error);
+  }
+});
