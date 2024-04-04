5275e2c012bae144df430253dc83b42382afa343
deepaksharmadk <dks847203@gmail.com>
2024-04-05 01:08:11 +0530

    added home layout  and hompage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Footer />} />
+        <Route path="/" element={<HomePage />}></Route>
       </Routes>
     </>
   );
