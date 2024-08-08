8eab7c771f2e9ef674ac93846f93a6f94738165f
deepaksharmadk <dks847203@gmail.com>
2024-08-09 01:32:43 +0530

    change the file

--- server/app.js
+++ server/app.js
@@ -17,4 +17,7 @@ app.use(cookieParser());
 //routes import
 import userRoutes from "./routes/user.route.js";
 app.use("/api/v1/user", userRoutes);
+app.listen(process.env.PORT, async () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
 export default app;
